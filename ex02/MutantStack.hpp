3304359e199d4738601b8d61038a381cbcffdf49
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-07-03 16:12:18 +0100

    Add MutantStack class with iterator support and update main for testing; enhance settings.json with additional file associations

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -11,9 +11,9 @@ class MutantStack : public std::stack<T>
         typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-        iterator begin() {return this->begin();};
-        iterator end() {return this->end();};
-        const_iterator begin() {return this->begin();};
-        const_iterator end() {return this->end();};
+        iterator begin() {return this->c.begin();};
+        iterator end() {return this->c.end();};
+        const_iterator begin() const {return this->c.begin();};
+        const_iterator end() const {return this->c.end();};
 
 };
