213e65d186eeac71134b70ceafb4d96bb720b5ee
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-07-09 21:05:51 +0100

    ex02 add tests & change makefile

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,7 @@
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
+
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
@@ -25,5 +26,34 @@ int main()
         ++it;
     }
     // std::stack<int> s(mstack);
+
+    // trying list container 
+    std::list <int> lstack;
+
+    lstack.push_back(5);
+    lstack.push_back(17);
+
+    std::cout << "list top : "<< mstack.top() << std::endl;
+
+    lstack.pop_back();
+    std::cout << "list " << mstack.size() << std::endl;
+
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+
+    lstack.push_back(0);
+
+    // while (std::list<int>::iterator lit = lstack.begin(); lit != lstack.end(); ++lit)
+    // {
+
+    // }
+
+    std::cout << "List contents:" << std::endl;
+    for (std::list<int>::iterator lit = lstack.begin(); lit != lstack.end(); ++lit) {
+        std::cout << *lit << std::endl;
+    }
+
+
     return 0;
 }
