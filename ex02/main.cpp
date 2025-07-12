2d05660f41234063cc456973108d20652db12ab2
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-07-12 16:25:26 +0100

    ex02 : fix main test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,8 +9,10 @@ int main()
     mstack.push(5);
     mstack.push(17);
     std::cout << mstack.top() << std::endl;
+
+    std::cout << "stack top : " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "stack size : " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
@@ -25,7 +27,6 @@ int main()
         std::cout << *it << std::endl;
         ++it;
     }
-    // std::stack<int> s(mstack);
 
     // trying list container 
     std::list <int> lstack;
@@ -33,21 +34,15 @@ int main()
     lstack.push_back(5);
     lstack.push_back(17);
 
-    std::cout << "list top : "<< mstack.top() << std::endl;
-
+    std::cout << "list top : " << lstack.back() << std::endl;
     lstack.pop_back();
-    std::cout << "list " << mstack.size() << std::endl;
+    std::cout << "list size : " << lstack.size() << std::endl;
 
     lstack.push_back(3);
     lstack.push_back(5);
     lstack.push_back(737);
 
     lstack.push_back(0);
-
-    // while (std::list<int>::iterator lit = lstack.begin(); lit != lstack.end(); ++lit)
-    // {
-
-    // }
 
     std::cout << "List contents:" << std::endl;
     for (std::list<int>::iterator lit = lstack.begin(); lit != lstack.end(); ++lit) {
